359442a03102aa920e759010a6cf91d608a50b09
natan borges <natanmelo5@hotmail.com>
2023-10-22 19:38:32 -0300

    test

--- drop-mail-nb/src/services/api.ts
+++ drop-mail-nb/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from 'axios'
 export const api = axios.create({
-    baseURL: `http://localhost:8080`,
+    baseURL: `https://cors-anywhere.herokuapp.com/corsdemo`,
     headers: {
         'Content-Type': 'application/json',
     },
